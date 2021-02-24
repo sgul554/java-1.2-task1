f1e436319348c845fbba462e1c9e127c57447966
Sasha Gulyaeva <sgulayeva@gmail.com>
2021-02-24 23:05:22 +0300

    Добавление файлов

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int count = 500_000_000;
+        int totalbalance = balance + count;
+        System.out.println(totalbalance);
+    }
+}
